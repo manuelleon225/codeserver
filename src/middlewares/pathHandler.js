869137bf93ec26070aa54a02885a9291e51fd26b
manuelleon225 <142952111+manuelleon225@users.noreply.github.com>
2024-04-01 19:36:20 -0500

    Revert "Challenge1"

--- src/middlewares/pathHandler.js
+++
@@ -1,8 +0,0 @@
-function pathHandler(req, res, next){
-    return res.json({
-        statusCoder: 404,
-        message: `${req.method} ${req.url} NOT FOUND PATH`
-    })
-}
-
-export default pathHandler 
