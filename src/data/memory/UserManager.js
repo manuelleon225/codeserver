25754ecbd92e5469521ed0c4df08d728beed6bc9
manuelleon225 <manueljhoanleonmantilla@gmail.com>
2024-04-01 19:26:39 -0500

    Se agrego la parte del user

--- src/data/memory/UserManager.js
+++ src/data/memory/UserManager.js
@@ -1,73 +1,82 @@
-import crypto from "crypto"
+import crypto from "crypto";
 
 class UserManager {
-    static #users = []
-create(data){
-     try {
-        if (!data.email || !data.password || !data.role) {
-            const error = new Error("Error: Faltan datos")
-            throw error
-        } else {
-            const newUser = {
-                id: crypto.randomBytes(12).toString("hex"),
-                photo: data.photo || "user_default.jpg",
-                email: data.email,
-                password: data.password,
-                role: data.role
-            }
-            UserManager.#users.push(user)
-            console.log("user createad");
-        }
-        
-     } catch (error) {
-        throw error
-     }
-}
-read(){
+  static #users = [];
+  create(data) {
     try {
-        if (UserManager.#users.length == 0) {
-            const error = new Error("No hay usuarios registrados")
-            throw error
-        } else {
-            return UserManager.#users
-        }
-        
+      if (!data.email || !data.password) {
+        const error = new Error("Error: Faltan datos");
+        throw error;
+      } else {
+        const newUser = {
+          id: crypto.randomBytes(12).toString("hex"),
+          photo: data.photo || "user_default.jpg",
+          email: data.email,
+          password: data.password,
+          role: data.role || "0",
+        };
+        UserManager.#users.push(newUser);
+        console.log("user createad (memory)");
+      }
     } catch (error) {
-        throw error
+      throw error;
     }
+  }
+  read() {
+    try {
+      if (UserManager.#users.length == 0) {
+        const error = new Error("No hay usuarios registrados");
+        throw error;
+      } else {
+        return UserManager.#users;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  readOne(id) {
+    try {
+      const userFound = UserManager.#users.find((user) => user.id == id);
+      if (!userFound) {
+        const error = new Error("No hay ningun usuario registrado con ese id");
+        throw error;
+      } else {
+        return userFound;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  destroy(id) {
+    try {
+      const userFound = UserManager.#users.find((user) => user.id == id);
+      if (!userFound) {
+        const error = new Error("No hay ningun usuario registrado con ese id");
+        throw error;
+      } else {
+        UserManager.#users = UserManager.#users.filter((user) => user.id != id);
+        return UserManager.#users;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  update(uid, data) {
+    try {
+      const userToUpdate = UserManager.#users.find((user) => user.id == uid);
+      if (userToUpdate) {
+        Object.assign(userToUpdate, data);
+        return userToUpdate;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
-readOne(id){
-    try{
-        const userFound = UserManager.#users.find((user) => user.id == id)
-        if(!userFound){
-            const error = new Error("No hay ningun usuario registrado con ese id")
-            throw error
-        } else {
-            return userFound
-        }
-    } catch(error){
-        throw error
-    }
-}
-destroy(id){
-    try{
-        const userFound = UserManager.#users.find((user) => user.id == id)
-        if(!userFound){
-            const error = new Error("No hay ningun usuario registrado con ese id")
-            throw error
-        } else {
-            UserManager.#users = UserManager.#users.filter((user) => user.id != id)
-            return UserManager.#users
-        }
-    } catch(error){
-        throw error
-    }
-}
-
-}
-
-const gestorDeUsuarios = new UserManager()
+/*const gestorDeUsuarios = new UserManager()
 gestorDeUsuarios.create({
     photo: "default_photo.jpg",
     email: "manuel_leon@gmail.com",
@@ -98,4 +107,4 @@ gestorDeUsuarios.create({
 
 console.log(gestorDeUsuarios.read())
 console.log(gestorDeUsuarios.destroy(2))
-console.log(gestorDeUsuarios.read()) 
+console.log(gestorDeUsuarios.read()) */
--- src/routers/api/index.api.js
+++ src/routers/api/index.api.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import productsRouter from "./products.api.js";
+import usersRouter from "./users.api.js";
+
 
 const indexApiRouter = Router()
 
 indexApiRouter.use("/products", productsRouter)
+indexApiRouter.use("/users", usersRouter)
+
 
 export default indexApiRouter
