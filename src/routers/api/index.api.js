f9da93fcac87573c9b7f30bda5400736bc28d66a
manuelleon225 <manueljhoanleonmantilla@gmail.com>
2024-04-08 18:59:48 -0500

    se agrego la parte del user

--- src/routers/api/index.api.js
+++ src/routers/api/index.api.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import productsRouter from "./products.api.js";
+import usersRouter from "./user.api.js";
 
 const indexApiRouter = Router()
 
 indexApiRouter.use("/products", productsRouter)
+indexApiRouter.use("/users", usersRouter)
 
 export default indexApiRouter
