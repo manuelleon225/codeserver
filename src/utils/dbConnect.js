f3190a7817776aae271e42dd24703868007fe83c
NachiLopez <nazarenolopez35@gmail.com>
2024-07-03 23:31:00 -0300

    created mocks products

--- src/utils/dbConnect.js
+++ src/utils/dbConnect.js
@@ -2,6 +2,7 @@ import { connect } from "mongoose";
 
 export async function dbConnect() {
     try {
+        console.log(process.env.MONGO_DATABASE_URI);
         await connect(process.env.MONGO_DATABASE_URI)
         console.log("mongo database connected successfully");
     } catch (error) {
